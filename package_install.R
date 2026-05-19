80d52f52d454269e01775f9afcd0bdcc7e38a9a3
Eli Felts <eli_felts@fws.gov>
2026-05-19 12:12:02 -0700

    initialize app with renv

---
+++ package_install.R
@@ -0,0 +1,9 @@
+install.packages(c("vroom","dataRetrieval","conflicted",
+                   "stringr","lubridate","dplyr",
+                   "readr","tidyr","forcats","purrr",
+                   "shiny","ggplot2","bslib","DT",
+                   "shinyWidgets","plotly","bsicons",
+                   "viridis","scales","fontawesome",
+                   "readr","ggokabeito","sf"))
+
+renv::snapshot()
